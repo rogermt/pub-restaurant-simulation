b0bb5045574bbed47cd14ae01f2475fe170bd3a7
Roger Taylor <rogermt@users.noreply.github.com>
2023-03-24 04:44:16 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pub-restaurant-simulation',
+    version='0.1',
+    author='Your Name',
+    author_email='your.email@example.com',
+    description='Simulates a fast food restaurant in a pub',
+    long_description='A detailed description of your project',
+    long_description_content_type='text/markdown',
+    url='https://github.com/yourusername/pub-restaurant-simulation',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    python_requires='>=3.6',
+)
 
