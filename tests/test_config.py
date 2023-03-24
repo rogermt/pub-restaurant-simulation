d8691b899c7a0e6dfb0f7e7592a5066b2e06f526
Roger Taylor <rogermt@users.noreply.github.com>
2023-03-24 04:30:36 +0000

    Update test_config.py

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,1 +1,13 @@
 
+import unittest
+from src import config
+
+
+class TestConfig(unittest.TestCase):
+    def test_default_config(self):
+        expected_config = {'n_customers': 100, 'sim_time': 3600, 'n_cashiers': 2}
+        self.assertEqual(config.get_config(), expected_config)
+
+
+if __name__ == '__main__':
+    unittest.main()
