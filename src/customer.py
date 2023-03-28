704a9f565a898d8048a53efa571f8bb9f0d4e5c1
Roger Taylor <rogermt@users.noreply.github.com>
2023-03-28 16:45:13 +0000

    Updated Customer class to Abstract created In-House Customer class

--- src/customer.py
+++ src/customer.py
@@ -1,66 +1,222 @@
-from typing import Optional
-import random
+from abc import ABC, abstractmethod
 import simpy
+import random
 from src.config import Config
 
-class Customer:
+
+class Customer(ABC):
     """
-    A class representing a customer in a restaurant simulation.
+    Abstract base class representing a customer in a restaurant simulation.
 
     Attributes:
-        env (simpy.Environment): The simulation environment.
-        id (int): The unique identifier for the customer.
-        restaurant (MockRestaurant): The restaurant the customer is visiting.
-        arrival_time (float): The time the customer arrived at the restaurant.
-        order_time (Optional[float]): The time the customer placed their order.
-        cook_time (Optional[float]): The time the customer's food started being cooked.
-        service_time (Optional[float]): The time the customer received their food.
-        config (Config): The configuration object for the simulation.
-    """
+    -----------
+    env : simpy.Environment
+        The simulation environment.
+    id : int
+        The unique identifier for the customer.
+    restaurant : MockRestaurant
+        The restaurant the customer is ordering from.
+    arrival_time : float
+        The time the customer arrived at the restaurant.
+    config : Config
+        The configuration object for the simulation.
 
-    def __init__(self, env: simpy.Environment, id: int, restaurant: 'MockRestaurant', 
-                 arrival_time: float, config: Config):
+    Methods:
+    --------
+    place_order() -> simpy.events.Event:
+        Sends an order request to the restaurant's order taker.
+    wait_for_food() -> simpy.events.Event:
+        Sends a request for food to the cook and waits for it to be prepared.
+    receive_food() -> simpy.events.Event:
+        Sends a request to the server for the customer's food and receives it.
+    leave() -> None:
+        Records the customer's departure from the restaurant and adds them to the restaurant's metrics.
+    """
+    
+    def __init__(self, env: simpy.Environment, id: int, restaurant: "Restaurant", arrival_time: float, config: Config):
+        """
+        Initializes a new instance of the Customer class.
+
+        Parameters:
+        -----------
+        env : simpy.Environment
+            The simulation environment.
+        id : int
+            The unique identifier for the customer.
+        restaurant : MockRestaurant
+            The restaurant the customer is ordering from.
+        arrival_time : float
+            The time the customer arrived at the restaurant.
+        config : Config
+            The configuration object for the simulation.
+        """
         self.env = env
         self.id = id
         self.restaurant = restaurant
         self.arrival_time = arrival_time
-        self.order_time: Optional[float] = None
-        self.cook_time: Optional[float] = None
-        self.service_time: Optional[float] = None
         self.config = config
+
+        self.order_time = None
+        self.cook_time = None
+        self.service_time = None
+
+    @abstractmethod
+    def place_order(self) -> simpy.events.Event:
+        """
+        Abstract method for placing an order with the restaurant's order taker.
+
+        Returns:
+        --------
+        simpy.events.Event:
+            A SimPy event representing the order request.
+        """
+        pass
+
+    @abstractmethod
+    def wait_for_food(self) -> simpy.events.Event:
+        """
+        Abstract method for sending a request for food to the cook and waiting for it to be prepared.
+
+        Returns:
+        --------
+        simpy.events.Event:
+            A SimPy event representing the food request.
+        """
+        pass
+
+    @abstractmethod
+    def receive_food(self) -> simpy.events.Event:
+        """
+        Abstract method for sending a request to the server for the customer's food and receiving it.
+
+        Returns:
+        --------
+        simpy.events.Event:
+            A SimPy event representing the food request.
+        """
+        pass
+
+    @abstractmethod
+    def leave(self) -> None:
+        """
+        Abstract method for recording the customer's departure from the restaurant and adding them to the restaurant's metrics.
+        """
+        pass
+
+
+
+class InHouseCustomer(Customer):
+    """
+    Class representing a customer who will dine in at the restaurant.
+
+    Attributes:
+    -----------
+    env : simpy.Environment
+        The simulation environment.
+    id : int
+        The unique identifier for the customer.
+    restaurant : MockRestaurant
+        The restaurant the customer is ordering from.
+    arrival_time : float
+        The time the customer arrived at the restaurant.
+    config : Config
+        The configuration object for the simulation.
+    order_time : Optional[float]
+        The time the customer placed their order.
+    cook_time : Optional[float]
+        The time the cook started preparing the customer's food.
+    service_time : Optional[float]
+        The time the customer received their food.
+
+    Methods:
+    --------
+    place_order() -> simpy.events.Event
+        Places an order with the restaurant's order taker.
+    wait_for_food() -> simpy.events.Event
+        Waits for the cook to prepare the customer's food.
+    receive_food() -> simpy.events.Event
+        Serves the food to the customer.
+    leave() -> None
+        Adds the customer to the restaurant's metrics.
+    """
+    def __init__(self, env: simpy.Environment, id: int, restaurant: "Restaurant", arrival_time: float, config: Config):
+        super().__init__(env, id, restaurant, arrival_time, config)
     
     def place_order(self) -> simpy.events.Event:
         """
         Places an order with the restaurant's order taker.
-        """
-        order_taker = self.restaurant.order_taker
-        with order_taker.request() as req:
-            yield req
-            self.order_time = self.env.now
-            yield self.env.timeout(random.uniform(self.config.mean_order_time-2, self.config.mean_order_time+2))
 
+        Returns:
+        --------
+        order : simpy.events.Event
+            The event that gets triggered when the order is taken by the restaurant's order taker.
+        """
+        # Send an order request to the restaurant's order taker
+        print("Sending order request to order taker...")
+        order_taker = self.restaurant.order_taker
+        with order_taker.request() as order:
+            yield order
+            
+            # Record the time the order was placed
+            print("Recording the time the order was placed...")
+            self.order_time = self.env.now
+
+            # Wait for the order to be taken
+            print("Waiting for the order to be taken...")
+            yield self.env.timeout(random.uniform(self.config.mean_order_time-2, self.config.mean_order_time+2))
+            
+            # Release the order taker resource
+            print("Releasing the order taker resource...")
+            self.restaurant.order_taker.release(order)
+
+       
+        
     def wait_for_food(self) -> simpy.events.Event:
         """
         Waits for the cook to prepare the customer's food.
-        """
-        cook = self.restaurant.cook
-        with cook.request() as req:
-            yield req
-            self.cook_time = self.env.now
-            yield self.env.timeout(random.uniform(self.config.mean_cook_time-3, self.config.mean_cook_time+3))
 
+        Returns:
+        --------
+        food : simpy.events.Event
+            The event that gets triggered when the cook finishes preparing the customer's food.
+        """
+        # Send a request for food to the cook
+        cook = self.restaurant.cook
+        with cook.request() as food:
+            yield food
+            
+            # Record the time the cook started cooking the customer's food
+            self.cook_time = self.env.now
+
+            # Wait for the cook to prepare the food
+            yield self.env.timeout(random.uniform(self.config.mean_cook_time-2, self.config.mean_order_time+2))
+
+            # Release the cook resource
+            self.restaurant.cook.release(food)
+    
     def receive_food(self) -> simpy.events.Event:
         """
-        Receives the customer's food from the server.
+        Serves the food to the customer.
+
+        Returns:
+        --------
+        serve : simpy.events.Event
+            The event that gets triggered when the customer is served their food.
         """
         server = self.restaurant.server
         with server.request() as req:
             yield req
+        
+            # Serve the food to the customer
+            yield self.env.timeout(random.uniform(self.config.mean_service_time-2, self.config.mean_order_time+2))
+    
+            # Record the time the customer received their food
             self.service_time = self.env.now
-            yield self.env.timeout(random.uniform(self.config.mean_service_time-1, self.config.mean_service_time+1))
-
+    
     def leave(self) -> None:
         """
-        Leaves the restaurant and adds the customer to the restaurant's metrics.
+        Adds the customer to the restaurant's metrics.
         """
+        # Add the customer to the restaurant's metrics
         self.restaurant.metrics.add_customer(self)
+
--- tests/test_customer.py
+++ tests/test_customer.py
@@ -1,29 +1,19 @@
-import simpy
 import unittest
-
+from simpy import Environment, Resource
 from typing import Any, List
-from src.customer import Customer
 from src.config import Config
+from src.customer import InHouseCustomer
 
-
-class MockOrderTaker(simpy.Resource):
+class MockOrderTaker(Resource):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
 
-    
-class MockRestaurant:
-    def __init__(self, mean_order_time: float, mean_cook_time: float, mean_serve_time: float) -> None:
-        self.mean_order_time: float = mean_order_time
-        self.mean_cook_time: float = mean_cook_time
-        self.mean_serve_time: float = mean_serve_time
-
-
-class MockCook(simpy.Resource):
+class MockCook(Resource):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
         
 
-class MockServer(simpy.Resource):
+class MockServer(Resource):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
 
@@ -36,44 +26,54 @@ class MockMetrics:
 
     def reset(self):
         self.customers = []
-        
 
-class TestCustomer(unittest.TestCase):
 
+class MockRestaurant:
+    def __init__(self, env: Environment, mean_order_time: float, mean_cook_time: float, mean_service_time: float, order_taker: Resource, cook: Resource, server: Resource, metrics: "MockMetrics" ) -> None:
+        self.env = env
+        self.mean_order_time: float = mean_order_time
+        self.mean_cook_time: float = mean_cook_time
+        self.mean_service_time: float = mean_service_time
+        self.order_taker = order_taker  
+        self.cook = cook
+        self.server = server
+        self.metrics = metrics
+
+
+class TestInHouseCustomer(unittest.TestCase):
     def setUp(self):
-        self.env = simpy.Environment()
-        
+        self.env = Environment()
         mean_order_time = 5
         mean_cook_time = 5
-        mean_serve_time = 5
-        self.restaurant = MockRestaurant(mean_order_time, mean_cook_time, mean_serve_time)
-        
-        self.restaurant.order_taker = MockOrderTaker(self.env,3)  
-        self.restaurant.cook = MockCook(self.env, 3)
-        self.restaurant.server = MockServer(self.env, 3)
-
+        mean_service_time = 5
+        self.restaurant = MockRestaurant(self.env, mean_order_time, mean_cook_time, mean_service_time, MockOrderTaker(self.env,3), MockCook(self.env, 3), MockServer(self.env, 3), MockMetrics())
         self.metrics = MockMetrics()
-        self.restaurant.metrics = self.metrics
-        config = Config()
-        self.customer = Customer(self.env, 1, self.restaurant, 0, config)
+        self.config = Config()
 
+        self.customer = InHouseCustomer(self.env, 1, self.restaurant, 0, self.config)
 
     def test_place_order(self):
-        self.env.process(self.customer.place_order())
-        self.assertTrue(self.customer.order_time is None)
-        self.env.run()
-        self.assertIsNotNone(self.customer.order_time)
-        self.assertTrue(self.customer.order_time >= 0)
-        self.assertTrue(self.customer.order_time >= self.customer.arrival_time)
+        # create a customer instance
+        customer = self.customer
 
-        
+        #customer place order
+        self.env.process(customer.place_order())
+        self.assertTrue(customer.order_time is None)
+        self.env.run()
+
+        #ensuring that the order was placed after the customer's arrival.
+        self.assertIsNotNone(customer.order_time)
+        self.assertTrue(customer.order_time >= 0)
+        self.assertTrue(customer.order_time >= customer.arrival_time)
+
     def test_wait_for_food(self):
         self.env.process(self.customer.wait_for_food())
+        self.assertTrue(self.customer.cook_time is None)
         self.env.run()
         self.assertIsNotNone(self.customer.cook_time)
         self.assertTrue(self.customer.cook_time >= 0)
         self.assertTrue(self.customer.cook_time >= self.customer.arrival_time)
-        
+    
 
     def test_receive_food(self):
         self.env.process(self.customer.receive_food())
@@ -83,9 +83,9 @@ class TestCustomer(unittest.TestCase):
         self.assertTrue(self.customer.service_time >= self.customer.arrival_time)
         
 
-    def test_leavetest_leave(self):
+    def test_leave(self):
         self.customer.leave()
-        self.assertTrue(len(self.metrics.customers) == 1)
+        self.assertTrue(len(self.restaurant.metrics.customers) == 1)
 
     def test_place_order_with_request(self):
         self.env.process(self.customer.place_order())
@@ -114,6 +114,4 @@ class TestCustomer(unittest.TestCase):
         self.assertTrue(self.customer.service_time >= 0)
         self.assertTrue(self.customer.service_time >= self.customer.order_time)
         
-
-if __name__ == '__main__':
-    unittest.main()
+    
